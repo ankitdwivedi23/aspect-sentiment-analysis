286d4dd9e0fd918ce2373c84039a78f4b1211fa3
Arunothia Marappan <armarapp@microsoft.com>
2019-11-14 13:27:48 -0800

    FeatureV1 Fit has been written, needs to be tested

--- progress/features.py
+++ progress/features.py
@@ -1,7 +1,20 @@
 import numpy as np
 import pandas as pd
+from pathlib import Path
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
+
+
+###########################################################
+# Helper Functions
+def getPostivePath(p):
+    return Path("./p-progress/Data/Train/Positive") / p
+def getNegativePath(p):
+    return Path("./p-progress/Data/Train/Negative") / p
+def getFileContent(fp):
+    with open(fp) as f:
+        content = f.read()
+    return content
 
 ############################################################
 # Abstract base class
@@ -34,10 +47,26 @@ class FeatureExtractorV0(FeatureExtractor):
 class FeatureExtractorV1(FeatureExtractor):
     def __init__(self):
         self.tfidfVectorizer = TfidfVectorizer(ngram_range=(1,3), min_df=0.001, stop_words='english')
+        self.CountVectorizer = CountVectorizer(stop_words="english", analyzer='word', ngram_range=(1, 3), min_df=0.001)
+        self.categories = ["Restaurants-food", "yelp-food"]
+        self.positive_files = map(getPositivePath, self.categories)
+        self.negative_files = map(getNegativePath, self.categories)
+        self.positive_frequencies = []
+        self.negative_frequencies = []
 
     def fit(self, X):
         self.tfidfVectorizer.fit([' '.join(X)])
-
+        for fp in self.positive_files:
+            content = getFileContent(fp)
+            frequencies = npasarray(self.CountVectorizer.fit_transform(content)).sum(axis=0)
+            featureNames = self.CountVectorizer.get_feature_names()
+            self.positive_frequencies.append(dict(zip(featureNames, frequencies)))
+        for fp in self.negative_files:
+            content = getFileContent(fp)
+            frequencies = npasarray(self.CountVectorizer.fit_transform(content)).sum(axis=0)
+            featureNames = self.CountVectorizer.get_feature_names()
+            self.negative_frequencies.append(dict(zip(featureNames, frequencies)))
+        
     def transform(self, X):
         return self.tfidfVectorizer.transform(X)
 ############################################################
