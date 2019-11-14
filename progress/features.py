f3b706a1f0824db6d09ef27a94e53402ca25027a
Arunothia Marappan <armarapp@microsoft.com>
2019-11-14 10:33:35 -0800

    Start

--- progress/features.py
+++ progress/features.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 
 ############################################################
 # Abstract base class
@@ -20,6 +21,23 @@ class FeatureExtractorV0(FeatureExtractor):
     def fit(self, X):
         self.tfidfVectorizer.fit([' '.join(X)])
     
+    def transform(self, X):
+        return self.tfidfVectorizer.transform(X)
+############################################################
+
+############################################################
+
+# Version-1 Feature extractor that uses scikit-learn's TfidfVectorizer
+# to create ngram features with tf-idf scores as feature values and 
+# custom implementation of PMI scores using scikit-learn's CountVectorizer
+# To evaluate the n-gram frequencies. 
+class FeatureExtractorV1(FeatureExtractor):
+    def __init__(self):
+        self.tfidfVectorizer = TfidfVectorizer(ngram_range=(1,3), min_df=0.001, stop_words='english')
+
+    def fit(self, X):
+        self.tfidfVectorizer.fit([' '.join(X)])
+
     def transform(self, X):
         return self.tfidfVectorizer.transform(X)
 ############################################################
