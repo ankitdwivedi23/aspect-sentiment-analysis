b556a0899e69b597dbee1630ffcaeacc2bc35216
Ankit Dwivedi <adwivedi@microsoft.com>
2019-11-14 12:54:58 -0800

    fix sentiment labels

--- progress/Preprocessing/processDataFromSemEval.py
+++ progress/Preprocessing/processDataFromSemEval.py
@@ -2,9 +2,12 @@ import sys
 import xml.etree.ElementTree as ET
 
 all_categories = ['ambience','anecdotes/miscellaneous','food','price','service']
-all_polarities = ['negative', 'positive', 'neutral', 'conflict']
+all_sentiments = ['negative', 'positive', 'neutral', 'conflict', 'na']
 
 def convertXmlToTsv(inputXmlFile, outputTsvFile):
+    # sentiment label for each aspect
+    sentimentLabels = {s[1] : s[0] for s in enumerate(all_sentiments)}
+    
     tree = ET.parse(inputXmlFile)
     root = tree.getroot()
     
@@ -14,18 +17,20 @@ def convertXmlToTsv(inputXmlFile, outputTsvFile):
             fields.append(sentence.attrib['id'])
             fields.append(sentence.find('text').text)
             categories = []
-            polarities = []
+            sentiments = []
+            categorySentiments = dict()
             for aspectCategory in sentence.find('aspectCategories').iter('aspectCategory'):
                 categories.append(aspectCategory.attrib['category'])
-                polarities.append(aspectCategory.attrib['polarity'])
+                sentiments.append(aspectCategory.attrib['polarity'])
+                categorySentiments[aspectCategory.attrib['category']] = aspectCategory.attrib['polarity']
             
             fields.append(",".join(categories))
+
             # boolean label for each category
             fields.append(",".join(["1" if c in categories else "0" for c in all_categories]))
-            
-            fields.append(",".join(polarities))
-            # boolean label for each polarity
-            fields.append(",".join(["1" if p in polarities else "0" for p in all_polarities]))
+            fields.append(",".join(sentiments))
+
+            fields.append(",".join([str(sentimentLabels[categorySentiments[c]]) if c in categories else str(sentimentLabels['na']) for c in all_categories]))
             f.write("\t".join(fields) + "\n")
     
 if __name__ == '__main__':
