3c366ed1133cc69ce99a41b4306701d8bd089781
Arunothia Marappan <armarapp@microsoft.com>
2019-12-11 19:39:35 -0800

    Parsing for PMI

--- progress/Preprocessing/getFilesForAspectBasedPMI.py
+++ progress/Preprocessing/getFilesForAspectBasedPMI.py
@@ -4,36 +4,28 @@ from pathlib import Path
 data_folder = Path("../data/")
 
 inputPath = data_folder / 'Restaurants_Train_All.tsv'
-outputPathPositive = data_folder / 'Lexicons' / 'Positive'
-outputPathNegative = data_folder / 'Lexicons' / 'Negative'
-outputPathNeutral = data_folder / 'Lexicons' / 'Neutral'
+outputPathPositive = data_folder / 'AspectLexicons' / 'Positive'
+outputPathNegative = data_folder / 'AspectLexicons' / 'Negative'
 
 aspects = all_categories = ['ambience','misc','food','price','service']
 
 data_positive ={}
 data_negative = {}
-data_neutral = {}
 
 for a in aspects:
     data_positive[a] = []
     data_negative[a] = []
-    data_neutral[a] = []
 
 with open(inputPath, encoding="utf-8") as f:
     for l in f:
         line = l.split('\t')
         aspectsPresent = line[3].split(',')
-        sentiment = line[5].split(',')
-        sentiment[4] = sentiment[4].replace('\n', '')
         #print(aspectsPresent)
-        #print(sentiment)
         for i,a in enumerate(aspectsPresent):
-            if sentiment[i] == '0': 
+            if a == '0': 
                 data_negative[aspects[i]].append(line[1])
-            elif sentiment[i] == '1':
-                data_positive[aspects[i]].append(line[1])
             else:
-                data_neutral[aspects[i]].append(line[1])
+                data_positive[aspects[i]].append(line[1])
 
 
 def writeToFile(fp, text_list): 
@@ -44,5 +36,4 @@ def writeToFile(fp, text_list):
 for i,a in enumerate(aspects):
     writeToFile(outputPathPositive / aspects[i], data_positive[aspects[i]])
     writeToFile(outputPathNegative / aspects[i], data_negative[aspects[i]])
-    writeToFile(outputPathNeutral / aspects[i], data_neutral[aspects[i]])
 
